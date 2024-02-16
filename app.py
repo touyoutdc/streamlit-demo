2ec7afea06d713999a6f811734114340a78e8e6e
to.yo <to.yo@tdc.co.jp>
2024-02-16 14:38:13 +0900

    main

--- app.py
+++ app.py
@@ -1,34 +1,16 @@
 import streamlit as st
-import plotly.graph_objects as go
 import plotly.express as px
+import pandas as pd
 
+st.set_page_config(layout="wide")
 
-months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-          'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-
-fig = go.Figure()
-fig.add_trace(go.Bar(
-    x=months,
-    y=[20, 14, 25, 16, 18, 22, 19, 15, 12, 16, 14, 17],
-    name='Primary Product',
-    marker_color='indianred'
-))
-fig.add_trace(go.Bar(
-    x=months,
-    y=[19, 14, 22, 14, 16, 19, 15, 14, 10, 12, 12, 16],
-    name='Secondary Product',
-    marker_color='lightsalmon'
-))
-
-# Here we modify the tickangle of the xaxis, resulting in rotated labels.
-fig.update_layout(barmode='group', xaxis_tickangle=-45)
+df = pd.read_csv('jepx.df_actual.csv')
+data_tokyo = df.query(
+        "area.str.contains('Area.TOKYO|Area.HOKKAIDO') and frame_start_time > '2024-01-01' and frame_start_time < '2024-01-06'"
+    )
+fig1 = px.bar(data_tokyo, x='frame_start_time', y='price', color='area', barmode='group', color_discrete_sequence=px.colors.qualitative.Vivid)
+fig2 = px.line(data_tokyo, x='frame_start_time', y='price', color='area', color_discrete_sequence=px.colors.qualitative.Vivid)
 
 # Plot!
-st.plotly_chart(fig, use_container_width=True)
-
-
-df = px.data.gapminder().query("continent == 'Europe' and year == 2007 and pop > 2.e6")
-fig2 = px.bar(df, y='pop', x='country', text='pop')
-fig2.update_traces(texttemplate='%{text:.2s}', textposition='outside')
-fig2.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
+st.plotly_chart(fig1, use_container_width=True)
 st.plotly_chart(fig2, use_container_width=True)
