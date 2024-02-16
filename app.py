94b0d1e4515d31f374d61fe6f1612f89b0a9f10b
to.yo <to.yo@tdc.co.jp>
2024-02-16 14:46:40 +0900

    main

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ import pandas as pd
 st.set_page_config(layout="wide")
 
 df = pd.read_csv('jepx.df_actual.csv')
-data_tokyo = df.query(
-        "area.str.contains('Area.TOKYO|Area.HOKKAIDO') and frame_start_time > '2024-01-01' and frame_start_time < '2024-01-06'"
-    )
-fig1 = px.bar(data_tokyo, x='frame_start_time', y='price', color='area', barmode='group', color_discrete_sequence=px.colors.qualitative.Vivid)
-fig2 = px.line(data_tokyo, x='frame_start_time', y='price', color='area', color_discrete_sequence=px.colors.qualitative.Vivid)
+data_tokyo = df.query("area.str.contains('Area.TOKYO|Area.HOKKAIDO') and frame_start_time > '2024-01-01' and frame_start_time < '2024-01-06'")
 
-# Plot!
+# 折れ線フラグ
+fig1 = px.line(data_tokyo, x='frame_start_time', y='price', color='area', color_discrete_sequence=px.colors.qualitative.Vivid)
 st.plotly_chart(fig1, use_container_width=True)
+
+# バーグラフ
+fig2 = px.bar(data_tokyo, x='frame_start_time', y='price', color='area', barmode='group', color_discrete_sequence=px.colors.qualitative.Vivid)
 st.plotly_chart(fig2, use_container_width=True)
